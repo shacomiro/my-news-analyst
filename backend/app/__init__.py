de1ad3afc408a25ad8e93b3c99825a5c35f6ad57
Sunghyun Lee <98dltjdgus@naver.com>
2025-06-16 21:32:13 +0900

    feat: DB 연결 초기화
    - backend\app\__init__.py: Flask 인스턴스가 DB에 접속할 수 있도록 수정.
    - backend\app\config.py: 기존 설정 삭제.
    - backend\config.py: 신규 설정 생성 및 DB 접속 추가.
    - backend\requirements.txt: DB 접속 관련 라이브러리 추가.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,31 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+import logging
 from app.routes import register_blueprints
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('config.DevelopmentConfig')
 
     # Initialize CORS
     CORS(app)
 
-    # Register Blueprints
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        try:
+            db.engine.connect()
+            app.logger.info("데이터베이스에 성공적으로 연결되었습니다.")
+        except Exception as e:
+            app.logger.error(f"데이터베이스 연결 실패: {e}")
+
     register_blueprints(app)
 
     return app
